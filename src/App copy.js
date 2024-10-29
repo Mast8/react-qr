e981facec86d81b060074b866482c3e0aac3184f
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-10-29 13:16:16 -0400

    some fixes

--- src/App copy.js
+++ src/App copy.js
@@ -1,43 +1,65 @@
-import QRCode from 'qrcode'
-import { useState } from 'react'
-import InputColor from "./components/InputColor"
+import axios from "axios";
+import { createContext, useState } from "react";
+import InputForm from "./components/InputForm";
+import QrCode from "./components/QrCode";
+
+// Create context
+export const InputContext = createContext();
 
 function App() {
-	const [url, setUrl] = useState('')
-	const [qr, setQr] = useState('')
+  const [inputValue, setInputValue] = useState({
+    url: '',
+    color: ''
+  });
+  const [response, setResponse] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-	const GenerateQRCode = () => {
-		QRCode.toDataURL(url, {
-			width: 800,
-			margin: 2,
-			color: {
-				dark: '#335383FF',
-				light: '#EEEEEEFF'
-			}
-		}, (err, url) => {
-			if (err) return console.error(err)
+  const config = {
+    headers: { Authorization: 'Bearer e0e29170-9e80-11ec-8ef8-55375f08d076' }
+  }
+  const bodyParameters = {
+    "colorDark": inputValue.color,
+    "qrCategory": "url",
+    "text": inputValue.url
+  }
+  const getQrCode = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.post(
+        'https://qrtiger.com/api/qr/static',
+        bodyParameters,
+        config
+      );
+      setResponse(res.data.url);
+    } catch(err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }
 
-			console.log(url)
-			setQr(url)
-		})
-	}
+  const value = {
+    inputValue,
+    setInputValue,
+    getQrCode,
+    response,
+    loading,
+    error
+  }
 
-	return (
-		<div className="app">
-			<h1>QR Generator</h1>
-			<InputColor />
-			<input 
-				type="text"
-				placeholder="e.g. https://google.com"
-				value={url}
-				onChange={e => setUrl(e.target.value)} />
-			<button onClick={GenerateQRCode}>Generate</button>
-			{qr && <>
-				<img src={qr} />
-				<a href={qr} download="qrcode.png">Download</a>
-			</>}
-		</div>
-	)
+  return (
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-24 md:pt-80 px-2">
+      <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
+        <div className="md:grid md:grid-cols-3">
+          <InputContext.Provider value={value}>
+            <InputForm />
+            <QrCode />
+          </InputContext.Provider>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import QRCode from 'qrcode'
-import { useState } from 'react'
+import { createContext, useState } from "react";
 import InputColor from "./components/InputColor"
 
+export const InputContext = createContext();
 
 function App() {
 
@@ -12,41 +13,54 @@ function App() {
 	//color 
 	const [inputValue, setInputValue] = useState({
 		url: '',
-		color: ''
+		color: '#fff'
 	  });
 
-
+	  console.log(inputValue.color);
 	const GenerateQRCode = () => {
-		QRCode.toDataURL(url, {
-			width: 800,
-			margin: 2,
-			/* color: {
-				dark: '#2bcb4b',
-				light: '#EEEEEEFF'
-			} */
-			color: inputValue.color
-		}, (err, url) => {
-			if (err) return console.error(err)
-
-			console.log(url)
-			setQr(url)
-		})
+		if( url.length > 0){
+			QRCode.toDataURL(url, {
+				width: 800,
+				margin: 2,
+				/* color: {
+					dark: '#2bcb4b',
+					light: '#EEEEEEFF'
+				} */
+				color: inputValue.color
+			}, (err, url) => {
+				if (err) return console.error(err)
+	
+				console.log(url)
+				setQr(url)
+			})
+		}
+		
 	}
+
+	const value = {
+		inputValue,
+		setInputValue,
+		
+	  }
 
 	return (
 		<div className="app">
 			<h1>QR Generator</h1>
-			<InputColor />
-			<input 
-				type="text"
-				placeholder="e.g. https://google.com"
-				value={url}
-				onChange={e => setUrl(e.target.value)} />
-			<button onClick={GenerateQRCode}>Generate</button>
-			{qr && <>
-				<img src={qr} />
-				<a href={qr} download="qrcode.png">Download</a>
-			</>}
+			
+			<InputContext.Provider value={value}>
+				<InputColor />
+				<input 
+					type="text"
+					placeholder="e.g. https://google.com"
+					value={url}
+					onChange={e => setUrl(e.target.value)} />
+					<button onClick={GenerateQRCode}>Generate</button>
+					{qr && <>
+						<img src={qr} />
+						<a href={qr} download="qrcode.png">Download</a>
+				</>}
+			</InputContext.Provider>
+			
 		</div>
 	)
 }
