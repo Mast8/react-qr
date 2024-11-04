fdf59ee8e159e0dfb5edc351503b58de25abaee0
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-11-04 11:41:57 -0500

    input validation

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
 	const [url, setUrl] = useState('')
  	const [qr, setQr] = useState('')
 
+	 const [message, setMessage] = useState("")
+
 	//one state
 	/* const [url, setUrl] = useState({
 		url: '',
@@ -23,25 +25,42 @@ function App() {
 
 	  //console.log(inputValue.color);
 	const GenerateQRCode = () => {
-		if( url.length > 0){
-			QRCode.toDataURL(url, {
-				width: 800,
-				margin: 2,
-				color: {
+		if(updateMessage(url)){
+			if( url.length > 0){
+				QRCode.toDataURL(url, {
+					width: 800,
+					margin: 2,
+					color: {
+						
+						dark: inputValue.color,
+						light: '#EEEEEEFF'
+					} 
 					
-					dark: inputValue.color,
-					light: '#EEEEEEFF'
-				} 
-				
-			}, (err, url) => {
-				if (err) return console.error(err)
-	
-				
-				setQr(url)
-			})
+				}, (err, url) => {
+					if (err) return console.error(err)
+		
+					
+					setQr(url)
+				})
+			}
 		}
 		
+		
 	}
+
+	function updateMessage(input) {
+		let passed = false;
+		let text = input.trim();
+		if(text == "") {
+		  setMessage("input can not be empty")
+		}else if( text.length < 3)
+				setMessage("Input needs to be at least 3 characters long")
+			  else{
+				passed = true;
+				setMessage("")
+			  }
+		return passed;
+	  }
 
 	const value = {
 		inputValue,
@@ -52,9 +71,9 @@ function App() {
 	return (
 		<div className="app">
 			<h1>QR Generator</h1>
-			
+
 			<InputContext.Provider value={value}>
-			
+			<span className="message" value={message}> {message} </span>
 				<InputColor />
 				<input 
 					type="text"
